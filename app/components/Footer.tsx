980221b3fd590caa7138618a588c1eb7347726a6
kelvin-0 <113959819+kelvin-0@users.noreply.github.com>
2023-06-30 12:58:51 +0700

    added help button

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,6 +9,20 @@ export default function Footer() {
             <h2 className="flex-grow-1 uppercase text-5xl font-bold">
               call for booking
             </h2>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-12 h-12 hover:cursor-help"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+              />
+            </svg>
             <div className="md:ml-auto rounded-full border border-slate-500     p-3">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -25,7 +39,7 @@ export default function Footer() {
                 />
               </svg>
             </div>
-            <p>+62 813-6093-1820</p>
+            <p>+62 xxx-xxxx-xxxx</p>
           </div>
           <hr className="mt-10 border-custom-gray" />
           <Link
